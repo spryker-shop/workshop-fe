eec644e72c475f5c5d5ae7e8c1ecba5c8e784834
ilmente <alessandro.bellini@spryker.com>
2019-02-21 16:40:10 +0100

    complete tasks

--- src/SprykerShop/Yves/WorkshopFe/Controller/IndexController.php
+++ src/SprykerShop/Yves/WorkshopFe/Controller/IndexController.php
@@ -10,52 +10,65 @@ namespace SprykerShop\Yves\WorkshopFe\Controller;
 use Spryker\Shared\Storage\StorageConstants;
 use SprykerShop\Yves\ShopApplication\Controller\AbstractController;
 use Spryker\Client\Kernel\Locator;
+use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @method \SprykerShop\Yves\WorkshopFe\WorkshopFeFactory getFactory()
- */
 class IndexController extends AbstractController
 {
-    /**
-     * @return \Spryker\Yves\Kernel\View\View
-     */
-    public function indexAction()
+    protected $baseDir = __DIR__;
+
+    public function indexAction(Request $request)
     {
-        $viewData = $this->executeIndexAction();
+        $task = $this->getTask($request);
+        $viewData = $this->executeIndexAction($task);
 
         return $this->view(
             $viewData,
             $this->getFactory()->getWorkshopFeWidgetPlugins(),
-            '@WorkshopFe/views/workshop/workshop.twig'
+            '@WorkshopFe/views/task-' . $task . '/task-' . $task . '.twig'
         );
     }
 
-    /**
-     * @return array
-     */
-    protected function executeIndexAction(): array
+    protected function getTask(Request $request)
     {
+        // 😎 look how nice this is...
+        $task = $request->get('task');
+
+        if (!$task) {
+           return 0;
+        }
+
+        return $task;
+    }
+
+    protected function executeIndexAction($task): array
+    {
+        $cars = $this->getAllFreakingCarsPlease();
+        $questions = $this->getSneakyQuestions($task);
+
+        return [
+            'cars' => $cars,
+            'questions' => $questions
+        ];
+    }
+
+    protected function getAllFreakingCarsPlease() {
         // 😭 😱 forgive me...
         $client = Locator::getInstance()->storage()->client();
 
         // 🚗💨
         $cars = [
-            $this->getTheFreakingCarPlease($client, 'product_concrete:en_us:1', 'product_abstract_image:en_us:1'),
-            $this->getTheFreakingCarPlease($client, 'product_concrete:en_us:2', 'product_abstract_image:en_us:2'),
-            $this->getTheFreakingCarPlease($client, 'product_concrete:en_us:3', 'product_abstract_image:en_us:3'),
-            $this->getTheFreakingCarPlease($client, 'product_concrete:en_us:4', 'product_abstract_image:en_us:4'),
+            $this->getOneFreakingCarPlease($client, 'product_concrete:en_us:1', 'product_abstract_image:en_us:1'),
+            $this->getOneFreakingCarPlease($client, 'product_concrete:en_us:2', 'product_abstract_image:en_us:2'),
+            $this->getOneFreakingCarPlease($client, 'product_concrete:en_us:3', 'product_abstract_image:en_us:3'),
+            $this->getOneFreakingCarPlease($client, 'product_concrete:en_us:4', 'product_abstract_image:en_us:4'),
         ];
 
-        return [
-            'cars' => $cars
-        ];
+        return $cars;
     }
 
-    protected function getTheFreakingCarPlease($client, $carId, $carImagesId) {
+    protected function getOneFreakingCarPlease($client, $carId, $carImagesId) {
         $car = $client->get($carId);
         $carTopSpeed = $car['attributes']['top_speed'];
-
-        // var_dump($car); die;
 
         $carImages = $client->get($carImagesId);
         $carImageUrl = $carImages['image_sets'][0]['images'][0]['external_url_large'];
@@ -64,5 +77,27 @@ class IndexController extends AbstractController
         $car['imageUrl'] = $carImageUrl;
 
         return $car;
+    }
+
+    protected function getSneakyQuestions($task) {
+        $path = $this->getSneakyQuestionsJsonFilePath($task);
+        $file = file_get_contents($path);
+        $questions = json_decode($file, true);
+
+        // 🤔
+        return $questions['questions'];
+    }
+
+    protected function getSneakyQuestionsJsonFilePath($task) {
+        $modulePath = realpath(
+            $this->baseDir .
+            DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . '..'
+        );
+
+        return $modulePath . '/data/workshop-fe/questions-' . $task . '.json';
     }
 }
